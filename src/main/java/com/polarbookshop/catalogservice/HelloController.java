c99f987d78a862fe17d708896c5e163f9382569a
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-12-30 14:46:15 +0100

    feat: add PolarProperties and test data loader

--- src/main/java/com/polarbookshop/catalogservice/HelloController.java
+++ src/main/java/com/polarbookshop/catalogservice/HelloController.java
@@ -1,14 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final PolarProperties polarProperties;
+
+    public HelloController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String greeting() {
-        return "Welcome to the book catalog!";
+        return polarProperties.getGreeting();
     }
 
 }
