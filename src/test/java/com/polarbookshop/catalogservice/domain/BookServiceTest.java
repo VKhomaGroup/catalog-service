4165ed1672ff173713e95b9e42052eeee5346603
vkhoma <16196348+HVV83@users.noreply.github.com>
2026-01-02 16:19:54 +0100

    feat: add publisher field to Book domain and persistence

--- src/test/java/com/polarbookshop/catalogservice/domain/BookServiceTest.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookServiceTest.java
@@ -1,12 +1,12 @@
 package com.polarbookshop.catalogservice.domain;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
@@ -23,7 +23,7 @@ class BookServiceTest {
     @Test
     void whenBookToCreateAlreadyExistsThenThrows() {
         var bookIsbn = "1234561232";
-        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 9.90);
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 9.90, "Polarsophia");
         when(bookRepository.existsByIsbn(bookIsbn)).thenReturn(true);
         assertThatThrownBy(() -> bookService.addBookToCatalog(bookToCreate))
                 .isInstanceOf(BookAlreadyExistsException.class)
